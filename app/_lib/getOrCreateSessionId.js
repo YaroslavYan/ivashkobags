713d2aee831db64075f24daf5bce5dad33c9c750
YaroslavYan <yaroslav5975@gmail.com>
2025-09-22 21:19:33 +0300

    Add files via upload

--- app/_lib/getOrCreateSessionId.js
+++ app/_lib/getOrCreateSessionId.js
@@ -1,13 +1,18 @@
 "use client";
 
-// export function getOrCreateSessionId() {
-//   if (typeof window === "undefined") return null; // Безпечна перевірка для SSR
+// import Cookies from "js-cookie";
 
-//   let sessionId = localStorage.getItem("sessionId");
+// export function getOrCreateSessionId() {
+//   let sessionId = Cookies.get("sessionId");
+
 //   if (!sessionId) {
 //     sessionId = crypto.randomUUID();
-//     localStorage.setItem("sessionId", sessionId);
+//     Cookies.set("sessionId", sessionId, {
+//       expires: 30, // зберігати 30 днів
+//       path: "/",
+//     });
 //   }
+
 //   return sessionId;
 // }
 
@@ -20,15 +25,11 @@ export function getOrCreateSessionId() {
     sessionId = crypto.randomUUID();
     Cookies.set("sessionId", sessionId, {
       expires: 30, // зберігати 30 днів
-      path: "/",
+      path: "/", // доступна на всьому сайті
+      sameSite: "Lax", // мінімізує попередження конфіденційності
+      secure: true, // працює лише по HTTPS
     });
   }
 
   return sessionId;
 }
-
-// Видаляти товар з кошика
-
-// Створити окрему сторінку для товару
-
-// Створити сторінку замовлення і сформувати в бд
